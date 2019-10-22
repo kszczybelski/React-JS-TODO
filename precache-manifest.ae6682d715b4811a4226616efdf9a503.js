8b993e61f0661090441e4175db5518425cb4c69e
kszczybelski <k.szczybelski@gmail.com>
2019-10-23 01:08:54 +0200

    React JS TODO

---
+++ precache-manifest.ae6682d715b4811a4226616efdf9a503.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a7671553a11d644b9f243e4dbc127c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "709ba4c9d78558f73b40",
+    "url": "/static/css/main.ae17764f.chunk.css"
+  },
+  {
+    "revision": "279440645029bc172962",
+    "url": "/static/js/2.72a4fe20.chunk.js"
+  },
+  {
+    "revision": "709ba4c9d78558f73b40",
+    "url": "/static/js/main.9d7c5a19.chunk.js"
+  },
+  {
+    "revision": "c1cdcb9d4958f317006b",
+    "url": "/static/js/runtime-main.dc30b89c.js"
+  }
+]);
